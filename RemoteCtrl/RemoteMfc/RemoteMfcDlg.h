3a60f91c007c88b96152ab3580fd24576c0b0a45
Liaodada1205 <951418947@qq.com>
2023-08-09 08:04:02 +0800

    项目代码的建立

---
+++ RemoteCtrl/RemoteMfc/RemoteMfcDlg.h
@@ -0,0 +1,34 @@
+﻿
+// RemoteMfcDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CRemoteMfcDlg 对话框
+class CRemoteMfcDlg : public CDialogEx
+{
+// 构造
+public:
+	CRemoteMfcDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_REMOTEMFC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
